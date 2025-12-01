6dbeaea516f69d9f09d9c6040b2aeb8fef32dd2a
JonesCabral <jonescabral.tech@gmail.com>
2025-12-01 16:47:41 -0300

    insert data file

--- demo/src/main/java/com/example/demo/CsvService.java
+++ demo/src/main/java/com/example/demo/CsvService.java
@@ -12,7 +12,6 @@ public class CsvService {
         }
 
         // 1. Identificar todas as chaves únicas (Cabeçalho)
-        // Usamos LinkedHashSet para tentar manter uma ordem de inserção razoável
         Set<String> headers = new LinkedHashSet<>();
         for (Map<String, Object> map : jsonList) {
             headers.addAll(map.keySet());
@@ -20,13 +19,31 @@ public class CsvService {
 
         StringBuilder csvContent = new StringBuilder();
 
-        // 2. Escrever o Cabeçalho
+        // 2. Escrever Cabeçalho
         csvContent.append(String.join(",", headers)).append("\n");
 
         
+        // 3. Escrever Linhas de Dados
+        for (Map<String, Object> row : jsonList) {
+            List<String> values = new ArrayList<>();
+            for (String header : headers) {
+                Object value = row.get(header);
+                String stringValue = (value != null) ? String.valueOf(value) : "";
+                values.add(escapeSpecialCharacters(stringValue));
+            }
+            csvContent.append(String.join(",", values)).append("\n");
+        }
 
         return csvContent.toString().getBytes();
     }
 
-   
+        //Tratar caracteres especiais no CSV 
+        private String escapeSpecialCharacters(String data) {
+            String escapedData = data.replaceAll("\\R", " "); // Remove quebras de linha no meio do dado
+            if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+                data = data.replace("\"", "\"\""); // Escapa aspas duplas
+                escapedData = "\"" + data + "\"";
+            }
+            return escapedData;
+        }
 }
